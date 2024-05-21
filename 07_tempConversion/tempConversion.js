d49d6233a002afe696b1dac5ff63ad60ae33458e
sjsusu <serena.shirlin@gmail.com>
2024-05-21 12:52:28 -0400

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreesF) {
+  let degreesC = (degreesF - 32) * (5 / 9);
+
+  if (degreesC % 1 !== 0) {
+    degreesC = Math.round(degreesC * 10) / 10;
+  }
+
+  return degreesC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesC) {
+  let degreesF = degreesC * (9 / 5) + 32;
+
+  if (degreesF % 1 !== 0) {
+    degreesF = Math.round(degreesF * 10) / 10;
+  }
+
+  return degreesF;
 };
 
 // Do not edit below this line
