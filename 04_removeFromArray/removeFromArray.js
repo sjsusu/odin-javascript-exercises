6356f6d13529ecd22418f45ae008401d046c63d2
sjsusu <serena.shirlin@gmail.com>
2024-05-21 11:55:08 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, arg1, arg2 = null, arg3 = null, arg4 = null) {
+    return array.filter(
+        (element) =>
+            !(element === arg1 || element === arg2 || element === arg3 || element === arg4)
+    );
 };
 
 // Do not edit below this line
