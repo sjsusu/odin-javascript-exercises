e93f8b9d1169c41b0ee22440470035fd34fe0bad
sjsusu <serena.shirlin@gmail.com>
2024-05-24 17:13:22 -0400

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (isNaN(num) || num < 0) {
+        return "OOPS";
+    }
 
+    let n = Number(num);
+
+    if (n===0) {
+        return 0;
+    } else if (n===1||n===2) {
+        return 1;
+    }
+
+    let fib_N_1 = 1;
+    let fib_N_2 = 1;
+    let fib_N = 0;
+
+    for (let i = 3; i<=n; i++){
+        fib_N = fib_N_1 + fib_N_2;
+        fib_N_1 = fib_N_2;
+        fib_N_2 = fib_N; 
+    }
+
+    return fib_N;
 };
 
 // Do not edit below this line
