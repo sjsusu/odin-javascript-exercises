38df1e1bd57de0c37ae2e9b9f73b7eff9128fa92
sjsusu <serena.shirlin@gmail.com>
2024-05-24 16:50:02 -0400

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let letters = [...string.toLowerCase()].filter((char) =>
+        ("a" <= char && char <= "z") || ("0" <= char && char <= "9"));
 
+    for (let i = 0; i < letters.length / 2; i++) {
+        if (letters[i] !== letters[letters.length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
