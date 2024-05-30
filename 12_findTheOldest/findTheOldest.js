d751bf4835eea3a411b0888620860eef4877e321
sjsusu <serena.shirlin@gmail.com>
2024-05-29 23:53:20 -0400

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = [null, 0];
+    let age = 0;
 
+    people.forEach(person => {
+
+        if (person.yearOfDeath !== undefined) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = new Date().getFullYear() - person.yearOfBirth;
+        }
+
+        if (age > oldest[1]) {
+            oldest = [person, age];
+        }
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
